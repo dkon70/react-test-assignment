17186254a28650f29394015738597ebe0f0b70c7
dkon70 <dkon70@inbox.ru>
2024-05-14 01:28:14 +0700

    feat: add search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { ChangeEvent, KeyboardEvent, useState } from "react";
+import SearchBar from "./widgets/SearchBar";
+
 function App() {
-  return <></>;
+  const [searchValue, setSearchValue] = useState('');
+
+  const searchChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  }
+
+  const searchSubmitHandler = () => {
+    console.log(searchValue);
+  }
+
+  const searchSubmitByKeyHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      console.log(searchValue);
+    }
+  }
+
+  return (
+    <main className="w-[100vw] h-[100vh]">
+      <SearchBar value={searchValue} onChange={searchChangeHandler} onKeyDown={searchSubmitByKeyHandler} onSubmit={searchSubmitHandler} />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/widgets/SearchBar.tsx
@@ -0,0 +1,23 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import SearchIco from '../assets/search.svg';
+import { ChangeEvent, KeyboardEvent } from "react";
+
+type SearchBarType = {
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown: (event: KeyboardEvent<HTMLInputElement>) => void;
+  onSubmit: () => void;
+}
+
+const SearchBar = (props: SearchBarType) => {
+  const { value, onChange, onKeyDown, onSubmit } = props;
+  return (
+    <div className="flex w-full max-w-sm items-center space-x-2 m-auto pt-5">
+      <Input type="text" placeholder="Введите название игры..." value={value} onChange={onChange} onKeyDown={onKeyDown} />
+      <Button type="submit" onClick={onSubmit}><img className="w-full h-full" src={SearchIco} alt="🔍" /></Button>
+  </div>
+  );
+}
+
+export default SearchBar;
